a1164849ce2727b22ecb61494bf53db8e25f7cfa
Daniel Karlsson <daniel.karlsson@powah.se>
2023-10-01 15:27:03 +0200

    Fix alignments

--- GrandLine/Assets/Scripts/Game.cs
+++ GrandLine/Assets/Scripts/Game.cs
@@ -1,9 +1,21 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public GameObject Player;
+
+    void Awake()
+    {
+        var player = Instantiate(Player, new Vector3Int(-2, 1), Quaternion.identity);
+        var playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
+        var cinematicScript = playerCamera.GetComponent<CinemachineVirtualCamera>();
+        cinematicScript.Follow = player.transform;
+        cinematicScript.LookAt = player.transform;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
