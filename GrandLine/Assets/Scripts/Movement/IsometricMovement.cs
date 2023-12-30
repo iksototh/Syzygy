6ec90656f88155f8a90dfb0ee513eb1fae62a4d4
Daniel Karlsson <daniel.karlsson@powah.se>
2023-12-30 20:32:05 +0100

    stuff

---
+++ GrandLine/Assets/Scripts/Movement/IsometricMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GrandLine.Movement
+{
+    public class IsometricMovement : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ GrandLine/Assets/Scripts/Systems/Pathfinding/IsometricPathFinder.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GrandLine
+{
+    public class IsometricPathFinder
+    {
+        const int MAX_CALCULATED_PATHS = 10000;
+
+        int ManhattanScore(Vector3Int position, Vector3Int target)
+        {
+            return Math.Abs(position.x - target.x) + Math.Abs(position.y - target.y);
+        }
+
+        Vector3Int GetCell(Vector2 target)
+        {
+            return Game.Instance.WorldMap.WorldToCell(target);
+        }
+        bool IsBlockedCell(int x, int y)
+        {
+            var colliderType = Game.Instance.WorldMap.CollisionMap.GetColliderType(new Vector3Int(x, y));
+            return colliderType != UnityEngine.Tilemaps.Tile.ColliderType.None;
+        }
+
+        bool IsBlockedCell(PathTile pathTile)
+        {
+            return IsBlockedCell(pathTile.X, pathTile.Y);
+        }
+
+        internal List<PathTile> CalculatePath(Vector2 position, Vector3Int targetPosition)
+        {
+            var startPosition = GetCell(position);
+            var target = new PathTile(0, targetPosition.x, targetPosition.y);
+            if (IsBlockedCell(target))
+            {
+                return null;
+            }
+
+            var start = new PathTile(ManhattanScore(startPosition, targetPosition), startPosition.x, startPosition.y);
+            var openList = new List<PathTile>() { start };
+            var finalPathList = new List<PathTile>();
+            var closedList = new List<PathTile>();
+
+            void AddPathPart(PathTile pathPart, PathTile parent)
+            {
+                if (IsBlockedCell(pathPart) && !Game.Instance.WorldMap.IsTown(pathPart.ToVector3Int()) || openList.Contains(pathPart) || closedList.Contains(pathPart))
+                {
+                    return;
+                }
+
+                pathPart.Parent = parent;
+                pathPart.Score = ManhattanScore(new Vector3Int(pathPart.X, pathPart.Y), new Vector3Int(target.X, target.Y));
+                openList.Add(pathPart);
+            }
+
+            var calculatedPaths = 0;
+            while (calculatedPaths < MAX_CALCULATED_PATHS || openList.Count == 0)
+            {
+                calculatedPaths = calculatedPaths + 1;
+
+                var startingCell = openList.OrderBy(cell => cell.Score).FirstOrDefault();
+                if (startingCell == null) break;
+
+                closedList.Add(startingCell);
+                openList.Remove(startingCell);
+
+                if (startingCell.X == target.X && startingCell.Y == target.Y) break;
+
+                var north = new PathTile(-1, startingCell.X, startingCell.Y + 1);
+                var northEast = new PathTile(-1, startingCell.X + 1, startingCell.Y + 1);
+                var east = new PathTile(-1, startingCell.X + 1, startingCell.Y);
+                var southEast = new PathTile(-1, startingCell.X + 1, startingCell.Y - 1);
+                var south = new PathTile(-1, startingCell.X, startingCell.Y - 1);
+                var southWest = new PathTile(-1, startingCell.X - 1, startingCell.Y - 1);
+                var west = new PathTile(-1, startingCell.X - 1, startingCell.Y);
+                var northWest = new PathTile(-1, startingCell.X - 1, startingCell.Y + 1);
+                AddPathPart(north, startingCell);
+                AddPathPart(east, startingCell);
+                AddPathPart(south, startingCell);
+                AddPathPart(west, startingCell);
+
+                //if (!IsBlockedCell(east) && !IsBlockedCell(north))
+                //{
+                //    AddPathPart(northEast, startingCell);
+                //}
+                //if (!IsBlockedCell(east) && !IsBlockedCell(south))
+                //{
+                //    AddPathPart(southEast, startingCell);
+                //}
+                //if (!IsBlockedCell(west) && !IsBlockedCell(south))
+                //{
+                //    AddPathPart(southWest, startingCell);
+                //}
+                //if (!IsBlockedCell(west) && !IsBlockedCell(north))
+                //{
+                //    AddPathPart(northWest, startingCell);
+                //}
+            }
+
+            if (calculatedPaths >= MAX_CALCULATED_PATHS - 1)
+            {
+                Debug.Log("Failed to find a path");
+
+                var lowestScore = closedList.OrderBy(cell => cell.Score).First().Score;
+                closedList = closedList.TakeWhile(cell => cell.Score > lowestScore).ToList();
+            }
+
+            var finalPath = closedList.Last();
+            void AddFinalPath(PathTile path)
+            {
+                if (path == null) return;
+                finalPathList.Add(path);
+
+                AddFinalPath(path.Parent);
+            }
+
+            AddFinalPath(finalPath);
+            finalPathList.Reverse();
+
+            return finalPathList;
+        }
+    }
+}
